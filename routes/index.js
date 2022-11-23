7f813bdbfacc569dbd6eb34257b0a384177af699
000raihan <000raihanahmad@gmail.com>
2022-11-23 12:32:15 +0600

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const base_url = "http://116.68.200.97:6055"
+
 const magazine_list = [
   {
     id:1,
-    url:"http://localhost:6055/magazine2022",
-    title: "Magazine 2022"
+    url:`${base_url}/magazine2022_february`,
+    thumb:"images/2022_1/Cronical-11-01.jpg",
+    title: "Chronicle",
+    sub_title: "CFebruary- April 2022"
   },
   {
     id:3,
-    url:"http://localhost:6055/magazine2023",
-    title: "Magazine 2023"
+    url:`${base_url}/magazine2022_august`,
+    thumb:"images/2022_2/CHRONICAL-12-01.jpg",
+    title: "Chronicle",
+    sub_title: "May - August 2022"
   },
 ]
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Radiant E Magazine', magazine_list:magazine_list});
 });
-router.get('/magazine2022', function (req, res, next) {
-  res.render('magazine_details');
+router.get('/magazine2022_february', function (req, res, next) {
+  res.render('magazine_details_february');
+});
+router.get('/magazine2022_august', function (req, res, next) {
+  res.render('magazine_details_august');
 });
 
 
